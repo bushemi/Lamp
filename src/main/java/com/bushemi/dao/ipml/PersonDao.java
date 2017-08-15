2725d3adb9dea5844d03907d2d6c20c9490d935b
bushemi <80959051487>
2017-08-16 01:08:45 +0300

    start to write tests

---
+++ src/main/java/com/bushemi/dao/ipml/PersonDao.java
@@ -0,0 +1,7 @@
+package com.bushemi.dao.ipml;
+
+/**
+ * Created by igor on 16.08.17.
+ */
+public class PersonDao {
+}
---
+++ src/test/java/com/bushemi/services/InterestServiceInterfaceTest.java
@@ -0,0 +1,36 @@
+package com.bushemi.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by igor on 16.08.17.
+ */
+public class InterestServiceInterfaceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addPlace() throws Exception {
+    }
+
+    @Test
+    public void addHobby() throws Exception {
+    }
+
+    @Test
+    public void getAllPeopleWithHobby() throws Exception {
+    }
+
+    @Test
+    public void getAllPeopleFromPlace() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
